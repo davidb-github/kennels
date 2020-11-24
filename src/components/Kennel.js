fc80f49221d5c157a2abe09288bf4818bae973e8
David Bing <davidb.devops@gmail.com>
2020-11-24 15:24:11 -0600

    implement ApplicationViews and NavBar | wrap Kennel in parent Router component

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -6,51 +6,18 @@ import { AnimalList }       from './animal/AnimalList'
 import { AnimalProvider }   from "./animal/AnimalProvider"
 import { EmployeeList }     from './employee/EmployeeList'
 import { EmployeeProvider } from "./employee/EmployeeProvider"
-import { Customer }         from "./customer/Customer"
 import { CustomerList }     from './customer/CustomerList'
 import { CustomerProvider } from './customer/CustomerProvider'
+// nav bar
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 
 // styles
 import "./Kennel.css"
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalProvider>
-                <AnimalList />
-            </AnimalProvider>
-        </article>
-
-        <h2>Employees</h2>
-        <article className="employees">
-            <EmployeeProvider>
-                <EmployeeList />
-            </EmployeeProvider>
-            
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            {/* delete Location x 2 and then add <LocationList /> */}
-            {/* anytime we want provider supplied data, we need to wrap that element in the provider <LocationProvider> tags and make it a child, also import provider at the top of this file.*/}
-            <LocationProvider>
-                <LocationList />
-            </LocationProvider>
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerProvider>
-                <CustomerList />
-            </CustomerProvider>
-        </article>
+         <NavBar />
+        <ApplicationViews />
     </>
 )
