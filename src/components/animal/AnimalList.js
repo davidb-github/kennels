95dd7417ef2e043dcfd7feb5e3272bea9e3cb445
David Bing <davidb.devops@gmail.com>
2020-11-25 09:45:00 -0600

    implement book 4 chapter 7

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,11 +2,15 @@ import React, { useContext, useEffect } from "react"
 import        { AnimalContext }       from './AnimalProvider'
 import        { Animal }              from './Animal'
 import                                     './Animal.css'
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
 
 
 export const AnimalList = () => {
     // This state changes when `getLocations()` is invoked below
     const { animals, getAnimals } = useContext(AnimalContext)
+    const { locations, getLocations } = useContext(LocationContext)
+    const { customers, getCustomers } = useContext(CustomerContext)
     
 
     /*
@@ -17,25 +21,33 @@ export const AnimalList = () => {
 //    will run only once because 2nd arg is a blank array/
     useEffect( () => {
         console.log("AnimalList: Initial render before data")
-        getAnimals()
+        getLocations()
+        .then(getCustomers)
+        .then(getAnimals)
     }, [])
 
-    /*
-        This effect is solely for learning purposes. The effect
-        it is responding to is that the animal state changed.
-    */
-//    This will run each time animal state changes.
-    useEffect( () => {
-        console.log("AnimalList: Animal state changed")
-        console.log(animals)
-    }, [animals])
+//     /*
+//         This effect is solely for learning purposes. The effect
+//         it is responding to is that the animal state changed.
+//     */
+// //    This will run each time animal state changes.
+//     useEffect( () => {
+//         console.log("AnimalList: Animal state changed")
+//         console.log(animals)
+//     }, [animals])
 
     // use {} anytime javascript is needed inside JSX
     return (
         <div className="animals">
-        {
-            animals.map(pet => <Animal key={pet.id} animal={pet} />)
-        }
+        {/* {console.log(animals, locations, customers)} */}
+          {
+            animals.map(animal => {
+              const owner = customers.find(c => c.id === animal.customerId)
+              const clinic = locations.find(l => l.id === animal.locationId)
+              // debugger
+              return <Animal key={animal.id} animal={animal} location={clinic} customer={owner} />
+            })
+          }
         </div>
-    )
-}
+      )
+    }
