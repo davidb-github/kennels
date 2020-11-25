6f22b752fbad7b5b4c75cd660ce17f3850dd831d
David Bing <davidb.devops@gmail.com>
2020-11-25 09:15:39 -0600

    update module: ApplicationView.js - add Customers/Employees links display their matching resources when clicked

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,9 +1,17 @@
+// react
 import React from "react"
 import { Route } from "react-router-dom"
+// providers
 import { LocationProvider } from "./location/LocationProvider"
 import { AnimalProvider } from "./animal/AnimalProvider"
+import { CustomerProvider } from './customer/CustomerProvider'
+import { EmployeeProvider } from './employee/EmployeeProvider'
+// lists
 import { LocationList } from "./location/LocationList"
 import { AnimalList } from "./animal/AnimalList"
+import { CustomerList } from './customer/CustomerList'
+import { EmployeeList }  from './employee/EmployeeList'
+
 
 export const ApplicationViews = (props) => {
     return (
@@ -21,6 +29,22 @@ export const ApplicationViews = (props) => {
                     <AnimalList />
                 </Route>
             </AnimalProvider>
+            
+            <CustomerProvider>
+                {/* Render the animal list when http://localhost:3000/customers */}
+                <Route path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
+            
+            <EmployeeProvider>
+                {/* Render the animal list when http://localhost:3000/employees */}
+                <Route path="/employees">
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
+            
+            {/* add list as descendent to the provider for customer and employees */}
         </>
     )
 }
