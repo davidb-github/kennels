80777e7ff4a036613bec800c9981aacbda3113ad
David Bing <davidb.devops@gmail.com>
2020-11-30 13:49:40 -0600

    update cards

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -5,7 +5,7 @@ export const Employee = ({ employee }) => (
     <section className="employee">
         <h3 className="employee__name">{employee.name}</h3>
         <div className="employee__location">Location: {employee.locationId}</div>
-        <div className="employee_animal">animalId: {employee.animalId}</div>
+        <div className="employee__animald">animalId: {employee.animalId}</div>
     </section>
 )
 
