930865d9e95e476865d9099d5dd70b0262b3fa79
David Bing <davidb.devops@gmail.com>
2020-11-30 11:03:08 -0600

    update module: Employee.js - add animalId to employee card

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -5,5 +5,8 @@ export const Employee = ({ employee }) => (
     <section className="employee">
         <h3 className="employee__name">{employee.name}</h3>
         <div className="employee__location">Location: {employee.locationId}</div>
+        <div className="employee_animal">animalId: {employee.animalId}</div>
     </section>
 )
+
+// add new values
