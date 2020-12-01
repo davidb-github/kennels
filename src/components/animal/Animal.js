66cfaa9239e3df69652558bf5cdba6cecc704ad3
David Bing <davidb.devops@gmail.com>
2020-12-01 13:40:47 -0600

    update module: Animal.js - remove breed from animal card

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -4,7 +4,7 @@ import "./Animal.css"
 export const Animal = ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">{animal.breed}</div>
+        {/* <div className="animal__breed">{animal.breed}</div> */}
         <div className="animal__location">Location: { location.name }</div>
         <div className="animal__owner">Customer: { customer.name }</div>
     </section>
